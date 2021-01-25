96feec9807cac10e1e1861c5e594c1e38d4ae36c
jpetryk <uin@kmd.dk>
2021-01-25 16:22:17 +0100

    fixed a typo with git config

--- src/properties/config.js
+++ src/properties/config.js
@@ -3,4 +3,4 @@ const cosmiconfig = require("cosmiconfig");
 const helmChartModule = cosmiconfig("helm-chart-publish").searchSync();
 const config = helmChartModule ? helmChartModule.config : {};
 
-module.export = config;
+module.exports = config;
